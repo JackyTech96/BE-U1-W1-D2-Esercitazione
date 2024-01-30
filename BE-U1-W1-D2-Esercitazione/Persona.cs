829cf768e8e6af2da17c80c382972ec6d7ae9bf6
Jacopo Guelpa <jacopo.guelpa@gmail.com>
2024-01-30 15:18:18 +0100

    Aggiungere i file di progetto.

---
+++ BE-U1-W1-D2-Esercitazione/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D2_Esercitazione
+{
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Età { get; set; }
+
+        public Persona(string nome, string cognome, int età) 
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Età = età;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        /*public void GetNome()
+        {
+        Console.WriteLine("Inserisci il nome della persona: ");
+        Nome=Console.ReadLine();
+        }
+        */
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        /* public void GetCognome()
+       {
+       Console.WriteLine("Inserisci il cognome della persona: ");
+       Cognome=Console.ReadLine();
+       }
+       */
+
+        public int GetEtà()
+        {
+            return Età;
+        }
+
+        /* public void GetEtà()
+       {
+       Console.WriteLine("Inserisci l'età della persona: ");
+       Età=Console.ReadLine();
+       }
+       */
+
+        public string GetDettagli() 
+        { 
+            return Nome + " " + Cognome + " " + Età;
+        }
+    }
+}
---
+++ BE-U1-W1-D2-Esercitazione/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D2_Esercitazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Luca", "Rossi", 27);
+            /* persona1.Nome = "Luca";
+            persona1.Cognome = "Rossi";
+            persona1.Età = 27;*/
+            Console.WriteLine("Nome: " + persona1.GetNome());
+            Console.WriteLine("Cognome: " + persona1.GetCognome());
+            Console.WriteLine("Età: " + persona1.GetEtà());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Dettagli persona: " + persona1.GetDettagli());
+            Console.ReadLine();
+        }
+    }
+}
